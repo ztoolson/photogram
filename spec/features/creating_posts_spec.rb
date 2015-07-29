1089fb0c489d5c569970d37c6f1ce3dda272f519
Zach Toolson <zach.toolson@gmail.com>
2015-07-28 20:40:53 -0600

    implement a integration tests for image validation when creating a post

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper.rb'
 require 'spec_helper'
 
 feature 'Creating posts' do
-  scenario 'can create a job' do
+  scenario 'can create a new post' do
     visit '/'
     click_link 'New Post'
     attach_file('Image', 'spec/files/images/awesome-cat.jpg')
@@ -11,4 +11,13 @@ feature 'Creating posts' do
     expect(page).to have_content('#awesomecat')
     expect(page).to have_css("img[src*='awesome-cat.jpg']")
   end
+
+  scenario 'a post needs an image to save' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'no picture because #YOLO'
+    click_button 'Create Post'
+    expect(page).to have_content('Halt, you fiend! You need an image to post here!')
+  end
 end
+
